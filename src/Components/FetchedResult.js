8f1aa3ef23400686186184da54a944774361f8a3
Arindam-7 <msaha6368@gmail.com>
2020-08-22 19:27:29 +0530

    improved DOM interface

--- src/Components/FetchedResult.js
+++ src/Components/FetchedResult.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled, {keyframes} from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCloud,
@@ -10,34 +11,99 @@ import {
   faSmog,
 } from '@fortawesome/free-solid-svg-icons';
 
+// animation
+const fadein = keyframes`
+to {
+    opacity: 1;
+    visibility: visible;
+  }
+`;
+
+// styled-components
+const Results = styled.div`
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    padding: 40px 0;
+    top: 25px;
+    opacity: 0;
+    visibility: hidden;
+    animation: ${fadein} 0.5s 1.4s forwards;
+`;
+
+const LocationAndDate = styled.div`
+    flex-basis: 100%;
+`;
+
+const Location = styled.h2`
+    display: block;
+    font-size: 30px;
+    color: white;
+    text-align: left;
+    font-weight: 600;
+    padding: 5px 0;
+`;
+
+const Date = styled.h2`
+    display: block;
+    font-size: 20px;
+    color: white;
+    text-align: left;
+    font-weight: 400;
+    padding: 5px 0;
+    transform: translateY(-15px);
+`;
+
+const Temp = styled.h3`
+    display: block;
+    font-size: 50px;
+    font-weight: 400;
+    color: #ffffff;
+`;
+
+const Desc = styled.h2`
+    display: block;
+    font-size: 20px;
+    color: white;
+    text-align: left;
+    font-weight: 400;
+    padding: 5px 0;
+    transform: translateY(-15px);
+    text-transform: capitalize;
+`;
 
 const FetchedResult = (props) => {
     return(
-        <div>
-            Fetching Data from the server...
-                <br></br>
-                City: {props.weather.city}
-                <br></br>
-                Country: {props.weather.country}
-                <br></br>
-                Temperature: {props.weather.temperature}
-                <br></br>
-                Date: {props.weather.date}
-                <br></br>
-                Description: {props.weather.description}
-                <br></br>
-                Highest Temperature: {props.weather.high}
-                <br></br>
-                Lowest Temperature: {props.weather.low}
-                <br></br>
-                Wind: {props.weather.wind}
+        <Results>
+            <LocationAndDate>
+                <Location>
+                    {props.weather.city}
+                    <br></br>
+                    {props.weather.country}
+                </Location>
+                <Date>
+                    {props.weather.date}
+                </Date>
+            </LocationAndDate>
+            <div>
+                <Temp>
+                    {props.weather.temperature}&#176;C
+                </Temp>
+                <Desc>
+                    {props.weather.description}
+                </Desc>
+            </div>
+                <div>
+                    Wind: {props.weather.wind}kph
+                </div>  
                 <br></br>
                 Sunrise: {props.weather.sunrise}
                 <br></br>
                 Sunset: {props.weather.sunset}
                 <br></br>
                 Humidity: {props.weather.humidity}%
-        </div>
+        </Results>
     );
 };
 
